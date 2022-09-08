19b38b09430dffb188219081e692a6c74b95077a
Artoym1234 <Artoym1234@mail.ru>
2022-09-08 18:36:32 +0300

    edit5v2

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,7 +11,7 @@ const minRange = 0;
 
 const makeProgressia = (firstNum, step, lengthProgression) => {
   const progression = [];
-  for (let i = 1; i < lengthProgression; i += step) {
+  for (let i = 1; i <= lengthProgression; i += step) {
     progression.push(firstNum + i);
   }
   return progression;
